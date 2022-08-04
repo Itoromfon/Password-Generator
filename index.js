cb6761544eb63d31ed488cc135401aac2dd0ce8b
Itoromfon <75027358+Itoromfon@users.noreply.github.com>
2022-08-05 00:46:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J",
+const letters = ["A","B","C","D","E","F","G","H","I","J",
 "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",
 "Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n",
-"o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", 
-"2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+const special = ["~","`","!","@","#",
 "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
 ",","|",":",";","<",">",".","?",
-"/"];
-let passwordLength = 15
+"/"]
 let inputOne = document.getElementById("input-one")
 let inputTwo = document.getElementById("input-two")
+let pswLengthEl = document.getElementById("password-lengths")
+let numbersEl = document.getElementById("numbers")
+let symbolsEl = document.getElementById("symbols")
 
 function randomPassword() {
     inputOne.textContent = ""
     inputTwo.textContent = ""
+    const passwordLength =+pswLengthEl.value;
+    const nums = numbersEl.checked ? [...numbers] : [];
+    const syms = symbolsEl.checked ? [...special] : [];
+    const availableChars = [...letters, ...nums, ...syms];
+    // console.log(availableChars)
+    let generatePasswordOne = "";
+    let generatePasswordTwo = "";
     for (let i = 0; i < passwordLength; i++) {
-        let generatePasswordOne = characters[ Math.floor( Math.random() * characters.length ) ]
-        let generatePasswordTwo = characters[ Math.floor( Math.random() * characters.length ) ]
-        inputOne.textContent += generatePasswordOne
-        inputTwo.textContent += generatePasswordTwo
+        generatePasswordOne += availableChars[ Math.floor( Math.random() * availableChars.length ) ]
+        generatePasswordTwo += availableChars[ Math.floor( Math.random() * availableChars.length ) ]
+        console.log(generatePasswordTwo)  
     }
+    inputOne.textContent += generatePasswordOne
+    inputTwo.textContent += generatePasswordTwo
 }
 
 function resetPassword() {
@@ -45,19 +56,80 @@ function resetPassword() {
 
 
 
-// let inputOne = document.getElementById("input-one")
-// let inputTwo = document.getElementById("input-two")
-// let passwordLength = 15
 
-// function randomPassword() {
-//     inputOne.textContent = ""
-//     inputTwo.textContent = ""
-//     for (let i = 0; i < passwordLength; i++) { 
-//         let generatePasswordOne = characters[ Math.floor( Math.random() * characters.length ) ]
-//         let generatePasswordTwo = characters[ Math.floor( Math.random() * characters.length ) ]
-//         inputOne.textContent += generatePasswordOne
-//         inputTwo.textContent += generatePasswordTwo
-//     }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
